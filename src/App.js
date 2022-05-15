c01782669ca6ddeefda8d19c32fdb929b69506b0
tanglero4 <96814274+tanglero4@users.noreply.github.com>
2022-05-15 15:27:30 -0400

    Added the GitHub repo URL

--- src/App.js
+++ src/App.js
@@ -5,7 +5,10 @@ export default function App() {
   return (
     <div className="App">
 Weather App
-    </div>
+<footer>This app was coded by <a href="https://aquamarine-lollipop-4f3091.netlify.app/" target="_blank" rel="noreferrer">Tiffany Anglero</a> and it is
+< a href="https://github.com/tanglero4/react-weather-app-homework" target="_blank" rel="noreferrer"> open-sourced on GitHub.</a>
+</footer>
+  </div>
   );
 }
 
